d91899320102f6e44d6967b9d6f5b8f9b1521f20
agconti <agc11d@gmail.com>
2014-10-11 22:49:16 -0400

    feat(custom/directives): added app level directives

---
+++ js/custom/directives.js
@@ -0,0 +1,14 @@
+// app directives
+
+function fadeIn(){
+
+}
+
+function slideChildrenIn(){
+
+}
+angular.module("appDirectives", [])
+
+       .directive("fadeIn", fadeIn)
+       .directive("slideChildrenIn", slideChildrenIn)
+;
