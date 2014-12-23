92af3ca300a2b419ec2758fb902d39c616b37e51
agconti <agc11d@gmail.com>
2014-12-23 10:09:58 -0500

    feat(templates/home): added email callout

--- js/custom/directives.js
+++ js/custom/directives.js
@@ -15,19 +15,30 @@ function fadeIn(Velocity){
 function slideChildrenIn(Velocity){
   return {
     restrict: 'A'
-  , link: function(scope, element){
-        Velocity(element.children(), 'transition.slideLeftIn', {
+  , link: function(scope, element, attrs){
+      var animationDelay = Number(attrs.delay) || 350
+
+      Velocity(element.children(), 'transition.slideLeftIn', {
           display: "inline"
-        , delay: 350
+        , delay: animationDelay
         , duration: 500
         , stagger: 100       
         , drag: true
       })
     }
-  };
+  }
 }
 
+function flicker(Velocity){
+  return {
+    restrict: 'A'
+  , link: function(scope, element){
+      Velocity(element, 'callout.flash', { delay: 3250 })
+    }
+  }
+}
 angular.module("appDirectives", [])
   .directive("fadeIn", ['Velocity', fadeIn])
   .directive("slideChildrenIn", ['Velocity', slideChildrenIn])
+  .directive("flicker", ['Velocity', flicker])
 })()
