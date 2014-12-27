3a2b62a5930203733bce5c7ddb705fc2fb7a4b1f
agconti <agc11d@gmail.com>
2014-12-26 23:01:38 -0500

    feat(custom/directives): layered animations wip

--- js/custom/directives.js
+++ js/custom/directives.js
@@ -1,12 +1,26 @@
 // app directives
 ;(function(){
+
+var preFadeAnimation = {opacity: 0}
+  , preFadeAnimationOptions = {
+      display: 'block'
+    , duration:0
+    , delay: 0
+  }
+
 function fadeIn(Velocity){
   return {
     restrict: 'A'
-  , link: function(scope, element){
+  , link: function(scope, element, attrs){
+      var delay = Number(attrs.delay) || 350
+      , duration = 1000 + delay
+      Velocity(element, preFadeAnimation, preFadeAnimationOptions).then(function(){
         Velocity(element, 'fadeIn', {
-          display: "block"
-        , duration: 1000
+            display: 'block'
+          , opactiy: 1 
+          , delay: delay
+          , duration: duration
+        })
       })
     }
   }
@@ -16,14 +30,15 @@ function slideChildrenIn(Velocity){
   return {
     restrict: 'A'
   , link: function(scope, element, attrs){
-      var animationDelay = Number(attrs.delay) || 350
+      var delay = Number(attrs.delay) || 350
+        , duration = 500 + delay
         , children = element.children()
-      Velocity(children, { opacity: 0}, {duration:0, delay: 0}).then(function(){
+      Velocity(children, preFadeAnimation, preFadeAnimationOptions).then(function(){
         Velocity(children, 'transition.slideLeftIn', {
-            display: "inline-block"
+            display: 'inline-block'
           , opactiy: 1
-          , delay: animationDelay
-          , duration: 500 + animationDelay
+          , delay: delay
+          , duration: duration
           , stagger: 100       
           , drag: true
         })
