b2a376014c7435c9e7b9434e43f6532b65829f83
agconti <agc11d@gmail.com>
2015-02-05 19:03:56 -0500

    feat(index): added basic bio
    feat(scss/bio): added basic styles
    
    feat(controllers/ScrollController): basic scroll functionality

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('sass', function () {
 
 gulp.task('js', function () {
   return gulp.src('js/**/*.js')
-    .pipe(changed('public'))
+    .pipe(changed('js/**/*.js'))
     .pipe(uglify())
     .pipe(concat("main.js"))
     .pipe(gulp.dest('public'))
