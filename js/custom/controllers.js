93a05e10999e0bb099f796dc12404b24e40b175b
agconti <agc11d@gmail.com>
2015-02-09 22:20:40 -0500

    fix(custom/controllers): removed refactored controller

--- js/custom/controllers.js
+++ js/custom/controllers.js
@@ -2,33 +2,6 @@
 ;(function(){
 'use strict'
 
-function ScrollController ($document, $timeout){
-
-  var atBio = false
-    , canScroll = true 
-
-  var canSwing = function(){
-    atBio = !atBio
-    canScroll = !canScroll
-    console.log('atbio %s', atBio)
-    $timeout(function(){
-      canScroll = !canScroll
-    }, 800)
-  }
-
-  var swingContentToBio = function(){
-    console.log('sdfas')
-    if (atBio && canScroll){
-      canSwing()
-      return $('.rotate-right').velocity('scroll')  
-    } else if(!atBio && canScroll) {
-      canSwing()
-      return $('.bio').velocity('scroll')
-    }
-  }
-  $document.bind('scroll', swingContentToBio)
-}
-
 function ContactFormController (Mandrill){
   this.pageTransitionClass = 'rotate-right'
   this.email
@@ -40,7 +13,6 @@ function ContactFormController (Mandrill){
 }
 
 angular.module("appControllers", ['appServices'])
-  .controller('ScrollController', ['$document', '$timeout', ScrollController])
   .controller('ContactFormController', ['Mandrill', ContactFormController])
 })()
 
